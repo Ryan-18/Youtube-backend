feff3e94ad49f4c6305f7a50464910109dff4214
RyanxGit <mdryanurrehman@gmail.com>
2024-06-07 20:22:51 +0530

    connection of Database

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express"
+    const app = express()
+; (async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+           console.error("ERROR", error)
+        throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`listning on port ${process.env.PORT}`)
+        })
+    } catch(error) {
+        console.error("ERROR", error)
+        throw error
+    }
+})()
